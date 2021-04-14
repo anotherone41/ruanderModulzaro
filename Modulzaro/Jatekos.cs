48d3c9d0a114a79e03aaf719e6831850e7105fe7
anotherone41 <anotherone41fod@hotmail.com>
2021-04-14 17:57:41 +0200

    1. feladat kész

---
+++ Modulzaro/Jatekos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulzaro
+{
+    class Jatekos
+    {
+        public string nev  { get; set; }
+        public string elso { get; set; }
+        public string utolso { get; set; }
+        public int suly { get; set; }
+        public int magassag { get; set; }
+
+        public Jatekos(string sor)
+        {
+            string[] sorok = sor.Split(';');
+            nev = sorok[0];
+            elso = sorok[1];
+            utolso = sorok[2];
+            suly = int.Parse(sorok[3]);
+            magassag = int.Parse(sorok[4]);
+        }
+    }
+}
---
+++ Modulzaro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulzaro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("balkezesek.csv");
+            List<Jatekos> jatekosok = new List<Jatekos>();
+            foreach (string sor in sorok.Skip(1))
+            {
+                jatekosok.Add(new Jatekos(sor));
+            }
+
+            //1. feladat
+            int jatekosokSzama = 0;
+            foreach (Jatekos jatekos in jatekosok)
+            {
+                jatekosokSzama++;
+            }
+            Console.WriteLine($"1. feladat: {jatekosokSzama} versenyzőről van adatunk");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
