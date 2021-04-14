d74ea6c1faecd1f76d7f502a8959372482aaf464
anotherone41 <anotherone41fod@hotmail.com>
2021-04-14 19:35:30 +0200

    kész

--- Modulzaro/Program.cs
+++ Modulzaro/Program.cs
@@ -37,17 +37,35 @@ namespace Modulzaro
                     Console.WriteLine($"\t {jatekos.nev}");
                 }
             }
-            /*
+            
             //3. feladat
             Console.WriteLine("3. feladat: Kérek egy nevet:");
             string bekertNev =Console.ReadLine();
-            bool van_e;
+            bool van_e = false;
             int j = 0;
-            while (j < elso1980 && van_e == false)
+            while(j < elso1980.Count && !van_e)
             {
-
+                if (elso1980[j].nev == bekertNev)
+                {
+                    van_e = true;
+                }
+                else
+                {
+                    j++;
+                }
             }
-            
+            if (van_e)
+            {
+                
+               
+                
+            }
+            else
+            {
+                Console.WriteLine("Hibás adat!");
+            }
+
+
             //4. feladat
             Console.Write("3. feladat: Kérek egy 1990 és 1999 közti évszámot:");
             int bekertEvaszam = Convert.ToInt32(Console.ReadLine());
@@ -62,7 +80,7 @@ namespace Modulzaro
 
                     foreach (Jatekos jatekos in jatekosok)
                     {
-                        if (jatekos.elso == bekertEvaszam)
+                        if (jatekos.elso.Contains(bekertEvaszam))
                         {
                             Console.WriteLine($"{jatekos.nev}, {jatekos.elso}, {jatekos.utolso}, {jatekos.suly}, {jatekos.magassag}");
                         }
@@ -75,19 +93,45 @@ namespace Modulzaro
                     int ujBekertEvszam = Convert.ToInt32(Console.ReadLine());
                     ujBekertEvszam = bekertEvaszam;
                     i++;
-                } 
-                */
+                }
+            }
 
-            //5. feladat
+             //5. feladat
+            List<int> evek = new List<int>();
+            foreach (Jatekos jatekos in jatekosok)
+            {
 
+                evek.Add(jatekos.elso);
+            }
+
+            // 6. feladat
+
+
+            //7. feladat 
+            Console.WriteLine("7. feladat: ");
+            HashSet<Jatekos> john = new HashSet<Jatekos>();
+            foreach (Jatekos jatekos in jatekosok)
+            {
+                if (jatekos.nev.Contains("John"))
+                {
+                    john.Add(jatekos);
+                }
+            }
+
+            foreach (Jatekos nevek in john)
+            {
+                Console.WriteLine(nevek.nev);
+            }
+
+            //8. feladat
             Console.ReadLine();
 
 
 
 
 
-        
-                
+
+               
         }   
     }
 }
